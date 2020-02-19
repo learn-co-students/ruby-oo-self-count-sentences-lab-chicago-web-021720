94d02c47e0edb00a602e349c9950369316f62a05
djoverbeatz29 <jroverby92@gmail.com>
2020-02-19 16:08:43 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
-require 'pry'
-
 class String
 
+  def self.end_with?(char)
+    self[-1] == char
+  end
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[!?.]+/).length
   end
+
 end
