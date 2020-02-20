1156d5102f965eb973ef8d275cba142e788b1e45
ellisandrews <ellisandrews1@gmail.com>
2020-02-19 18:25:37 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
+
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    potential_sentences = self.split(/\.|\?|\!/)
+    potential_sentences.filter{ |potential_sentence| potential_sentence.length > 1}.length
   end
 end
